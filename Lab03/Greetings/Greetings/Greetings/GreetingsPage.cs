05b9caf6bd7f57d000badf42898fbe7908027652
Daniel Tadeu <danieltadeu2.dt@gmail.com>
2017-02-05 20:02:03 -0200

    Commit do código do lab03

---
+++ Lab03/Greetings/Greetings/Greetings/GreetingsPage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Greetings
+{
+    public class GreetingsPage : ContentPage
+    {
+        public GreetingsPage()
+        {
+            var MyLabel = new Label();
+            MyLabel.Text = "Greetings, Xamarin.Forms";
+            this.Content = MyLabel;
+
+            //solução 1
+            //Padding = new Thickness(0, 20, 0, 0);
+
+            //solução 2
+            Padding = Device.OnPlatform<Thickness>(
+                new Thickness(0, 20, 0, 0),
+                new Thickness(0),
+                new Thickness(0));
+
+            MyLabel.HorizontalOptions = LayoutOptions.Center;
+            MyLabel.VerticalOptions = LayoutOptions.Center;
+
+            MyLabel.HorizontalTextAlignment = TextAlignment.Center;
+            MyLabel.VerticalTextAlignment = TextAlignment.Center;
+        }
+    }
+}
